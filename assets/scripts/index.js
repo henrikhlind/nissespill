5148db2199942ffa9dc97458f3b543a0b83b61b3
grindser <henrik.haugen.lindstad@gmail.com>
2022-12-19 11:05:14 +0100

    Initial commit

---
+++ assets/scripts/index.js
@@ -0,0 +1,60 @@
+const canvas = document.getElementById('game-canvas');
+const ctx = canvas.getContext('2d');
+
+const santaImage = new Image();
+santaImage.src = '../assets/img/nisse.png'; // Replace with the path to your Santa image file
+
+let santaHeight = window.innerHeight / 3;
+let santaWidth = window.innerWidth / 5;
+let santaX = window.innerWidth / 2.5;
+let santaY = window.innerHeight - santaHeight;
+let santaSpeed = 200;
+let flipped = false;
+
+
+function drawSanta() {
+  ctx.drawImage(santaImage, santaX, santaY, santaWidth, santaHeight);
+}
+
+function moveSanta(event) {
+  if (event.key === 'ArrowRight') {
+    santaX += santaSpeed;
+    if(!flipped) {
+        santaImage.src = '../assets/img/nisse-flipped.png';
+        flipped = true;
+    }
+  } else if (event.key === 'ArrowLeft') {
+    santaX -= santaSpeed;
+    if(flipped) {
+        santaImage.src = '../assets/img/nisse.png';
+        flipped = false;
+    }
+  }
+}
+
+document.addEventListener('keydown', moveSanta);
+
+function updateGame() {
+  // Update game state, such as checking for collisions or moving non-player objects
+  // ...
+
+  // Clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw Santa
+  drawSanta();
+
+  // Request next frame
+  requestAnimationFrame(updateGame);
+}
+
+updateGame();
+
+window.addEventListener('resize', resizeCanvas, false);
+
+resizeCanvas();
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
